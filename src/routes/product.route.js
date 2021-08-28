da5eb35ca59eeabea28fd09668fe841245f749a1
Pantavit Hengnalen <tein1142@hotmail.com>
2021-08-28 14:54:34 +0700

    [ADD] CRUD add product

--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -3,6 +3,32 @@ const productType = require('../models/products/productType.model')
 const express = require('express')
 const router = new express.Router()
 const { authUser } = require('../middleware/auth.middleware')
+
+router.post('/addProduct', authUser, async(req, res) => {
+    const checkKeyBody = Object.keys(req.body)
+    const allowedKey = ['prodName', 'manufacDate', 'prodDescription', 'price', 'productType']
+    const inValidKey = checkKeyBody.every((checkKeyBody) => {
+        return allowedKey.includes(checkKeyBody)
+    })
+    if (!inValidKey) {
+        await res.status(500).send('Invalid key!')
+    }
+    try {
+        await Product.create({
+            prodID: req.body.prodID,
+            prodName: req.body.prodName,
+            manufacDate: req.body.manufacDate,
+            prodDescription: req.body.prodDescription,
+            price: req.body.price,
+            ownerID: req.user.userID,
+            productType: req.body.productType
+        })
+        res.status(200).send('Product Created!')
+    } catch (error) {
+        res.status(500).send(error)
+    }
+})
+
 router.get('/allProduct', async(req, res) => {
     try {
         const products = await Product.findAll()
@@ -12,17 +38,16 @@ router.get('/allProduct', async(req, res) => {
         res.status(500).send(error)
     }
 })
-
 router.get('/product', authUser, async(req, res) => {
-    // try {
-    const product = await Product.findOne({
-        where: { ownerID: req.user.userID },
-    }, )
-    if (!product) res.send('Product not found!')
-    await res.send(product)
-        // } catch (error) {
-        //     res.status(500).send(error)
-        // }
+    try {
+        const product = await Product.findAll({
+            where: { ownerID: req.user.userID },
+        }, )
+        if (!product) res.send('Product not found!')
+        await res.send(product)
+    } catch (error) {
+        res.status(500).send(error)
+    }
 })
 
 router.get('/typeProd', async(req, res) => {
