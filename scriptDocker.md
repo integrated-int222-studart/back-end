0e4ce9bd74ceb75a9c6177fb6f0c3628012678be
tein1142 <tein1142@hotmail.com>
2021-09-07 15:13:56 +0700

    [ADD] docker-compose

--- scriptDocker.md
+++ scriptDocker.md
@@ -4,3 +4,10 @@
 2. docker run --name studart-project -d --env-file .env -p 3000:3000 tein1142/studart-project
 
 docker container run --network studart-mysql --name studart-project -d --env-file .env -p 3000:3000 -d tein1142/studart-project
+
+<h1>Create Mysql network
+docker network create studart-network  
+container run --name studart-mysql --network studart-network -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=Studart_System -d mysql
+
+<h1>Run image app in network
+docker container run --network studart-network --name studart-project -p 3000:3000 -d studart-project
--- src/database/sequelize.js
+++ src/database/sequelize.js
@@ -8,7 +8,22 @@ const sequelize = new Sequelize(process.env.DATABASE, process.env.USER, process.
         freezeTableName: true
     }
 });
+// const sequelize = new Sequelize(process.env.CONECTION, {
+//     define: {
+//         freezeTableName: true
+//     }
+// })
 // sequelize.sync({ force: true }).then(() => {
 //     console.log("Drop and re-sync db.");
 // });
+const testConnection = async()=>{
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+      } catch (error) {
+        console.error('Unable to connect to the database:', error);
+      }
+}
+testConnection()
+
 module.exports = sequelize
