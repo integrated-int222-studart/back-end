6b76d9dcfddfa3fd3f0f84700e173044ee8188ec
Pantavit.H <tein1142@hotmail.com>
2021-11-07 17:09:23 +0700

    [UPDATE] API favorite

--- src/routes/products/favorite.route.js
+++ src/routes/products/favorite.route.js
@@ -2,6 +2,11 @@ const express = require('express')
 const router = new express.Router()
 const { authUser } = require('../../middleware/auth.middleware')
 const Favorite = require('../../models/ManyToMany/favorite.model')
+const Product = require('../../models/products/product.model')
+const productType = require('../../models/products/productType.model')
+const Style = require('../../models/products/style.model')
+const User = require('../../models/user/User.model')
+const Image = require('../../models/products/images.model')
 router.post('/addFavorite/:prodId', authUser, async (req, res) => {
     try {
         const productId = parseInt(req.params.prodId) 
@@ -33,12 +38,29 @@ router.delete('/deleteFavorite/:prodId',authUser, async(req,res) =>{
 router.get('/favorite/:userId',async (req,res)=>{
     try {
         const userID = req.params.userId
-        const favorites = await Favorite.findAndCountAll({
-            where:{
-                userID,
-            }
+        const favoriteUser = await User.findOne({
+            as: 'users',
+            attributes: {exclude:["userID","username","password","email","firstName","lastName","description","status","school","imageData","imageName","imageType","imageURL"]},
+            where: {
+                userID
+            },
+            include: [{
+                model: Product,
+                as: 'productFavorite',
+                // attributes: { exclude: ['favorlite'] },
+                include: [{
+                    model: Image,
+                    attributes: { exclude: ['data'] }
+                },{
+                    model: productType,
+                }, {
+                    model: Style,
+                    as: 'style'
+                    // attributes: { exclude: ['productstyles'] },
+                }],
+            },]
         })
-        res.status(200).send(favorites)
+        res.status(200).send(favoriteUser)
     } catch (error) {
         res.status(500).send({ error: error.message })
     }
